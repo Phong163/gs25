932ecd9accf1bb00ff9cb9a758b51280fb6f9e6b
Thai Hoang <thai.hoangvan@cxview.ai>
2025-08-12 12:00:04 +0700

    update 12_8

---
+++ config/params_metric5.py
@@ -0,0 +1,102 @@
+camera_configs = {
+    "vn300":{
+        "box_id":"d2329d74-0219-47d3-ae75-32c12fe95723",
+        "cameras":{
+            0: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/101",
+                "zone_1": [
+                    (0.504, 0.460),  # Điểm 1
+                    (0.01, 0.764),  # Điểm 2
+                    (0.066, 0.996),  # Điểm 3
+                    (0.656, 0.996),   # Điểm 4
+                    (0.460, 0.77),  # Điểm 5
+                    (0.664, 0.577), # # Điểm 6
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            1: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/701",
+                "zone_1": [
+                    (0.504, 0.410),  # Điểm 1
+                    (0.368, 0.418),  # Điểm 2
+                    (0.345, 0.996),  # Điểm 3
+                    (0.620, 0.996),   # Điểm 4
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            2: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1001",
+                "zone_1": [
+                    (0.597, 0.481),  # Điểm 1
+                    (0.445, 0.468),  # Điểm 2
+                    (0.362, 0.995),  # Điểm 3
+                    (0.637, 0.995)   # Điểm 4
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            3: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1101",
+                "zone_1": [
+                    (0.6, 0.345),  # Điểm 1
+                    (0.279, 0.354),  # Điểm 2
+                    (0.139, 0.993),  # Điểm 3
+                    (0.697, 0.993)   # Điểm 4
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            4: {
+                "rtsp_url": "rtsp://cxview:gs252525@42.1.117.16:554/Streaming/Channels/1401",
+                "zone_1": [
+                    (0.862, 0.398),  # Điểm 1
+                    (0.637, 0.354),  # Điểm 2
+                    (0.191, 0.993),  # Điểm 3
+                    (0.847, 0.993)   # Điểm 4
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+        }
+    },
+    "vn316":{
+        "box_id":"d2329d74-0219-47d3-ae75-32c12fe95723",
+        "cameras":{
+            1: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/101",
+                "zone_1": [
+                    (0.54, 0.24), (0.42, 0.24), (0.35, 0.91), (0.58, 0.92)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            3: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/301",
+                "zone_1": [
+                    (0.33, 0.36), (0.19, 0.38), (0.18, 0.99), (0.42, 1.0)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            4: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/401",
+                "zone_1": [
+                    (0.86, 0.38), (0.74, 0.36), (0.61, 0.99), (0.86, 0.99)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            },
+            5: {
+                "rtsp_url": "rtsp://cxview:gs252525@115.79.136.220:554/Streaming/Channels/1101",
+                "zone_1": [
+                    (0.74, 0.25), (0.61, 0.25), (0.58, 0.52), (0.78, 0.54)
+                ],
+                "track_person": {},
+                "current_frame": 0
+            }
+        }
+    },
+}
+
